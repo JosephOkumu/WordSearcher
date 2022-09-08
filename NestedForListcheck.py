6e7f96985d544774f6b484644e2b10e5f15d066c
javez denzel <jokumu25@gmail.com>
2022-09-08 18:07:09 +0300

    committed

---
+++ NestedForListcheck.py
@@ -0,0 +1,8 @@
+list1 = ["Alison", "Mina", "Leticia", "Elaine", "Sonny", "Benito"]
+list2 = ["Yuri", "Andrew", "Mina", "Elaine", "Charles", "Alison"]
+for name1 in list1:
+    for name2 in list2:
+        #This is just a print debug statement to see how the code runs.
+        #print(name1, name2)
+        if name1 == name2:
+             print(name1)
